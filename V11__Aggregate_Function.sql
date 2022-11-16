ec53612b21d9cca1c47c0325fa2b4c35b728bb0d
Roma_Gin <shshmsnem@yandex.ru>
2022-11-16 11:20:07 +0300

    add task 11

---
+++ V11__Aggregate_Function.sql
@@ -0,0 +1,9 @@
+SELECT count(nickname) FROM user_page;
+
+SELECT max(rating) FROM stats;
+
+SELECT min(matches_played) FROM stats;
+
+SELECT sum(teammate_rating) FROM best_teammate;
+
+SELECT avg(average_kd) FROM stats;
